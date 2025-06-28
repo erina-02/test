65b1ec5f580194d47e560a2b7368636dac4dade1
erina <t.sky.2vv@outlook.jp>
2025-06-29 01:24:30 +0900

    ファイル名変更

---
+++ start-folder/JavaScriptWork/1_3.js
@@ -0,0 +1,11 @@
+const getTotalPrice = (price, quantity) => price * quantity;
+
+const addTax = total =>  * 1.1;
+
+const total = getTotalPrice(1000, 2); // → 2000
+
+console.log(`税込金額は${total}円です`);
+
+const  = addTax(total);     // → 2200
+
+console.log(`税込金額は${taxedTotal}円です`);
